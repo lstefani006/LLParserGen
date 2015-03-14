2d2198689f76b9a57e945ebeb73ef2e51ff44afd
leo <leonardo.stefani.ge@gmail.com>
2015-03-14 07:40:46 +0100

    ddd

--- LLParserGenTest/main.cs
+++ LLParserGenTest/main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using LLParserLexerLib;
 
 namespace LLParserGenTest
@@ -7,32 +8,61 @@ namespace LLParserGenTest
 	{
 		public static void Main(string [] args)
 		{
-			LexReader rd = null;
-			try {
-				if (args.Length == 1)
-					rd = new LexReader(args[0]);
-				else
-					rd = new LexReader(Console.In, "stdin");
+			using (var rd = (args.Length == 1 ? new LexReader(args[0]) : new LexReader(Console.In, "stdin"))) {
 
 				var p = new MParser();
-				var e = p.Start(rd);
+				var s = p.Start(rd);
 
 				using (Context ctx = new Context())
 				{
-				Console.WriteLine(e.Exec(ctx));
+					s.GenCode(ctx);
+					Console.WriteLine();
 				}
-			}
-			finally {
-				if (rd != null)
-					rd.Dispose();
 			}
 		}
 	}
 
+	public abstract class StmtRoot : IAST {
+		public abstract void GenCode(Context ctx);
+	}
 
-	public abstract class ExprRoot : IAST
-	{
-		public abstract int Exec(Context ctx);
+	public class StmtIf : StmtRoot {
+		ExprRoot e;
+		StmtRoot s;
+		public StmtIf(ExprRoot e, StmtRoot s) { this.e = e; this.s = s; }
+		public override void GenCode(Context ctx) {
+		}
+	}
+	public class StmtExpr : StmtRoot {
+		ExprRoot e;
+		public StmtExpr(ExprRoot e) { this.e = e; }
+		public override void GenCode(Context ctx) {
+		}
+	}
+
+
+	public abstract class ExprRoot : IAST {
+		public abstract string EvalLeft(Context ctx);
+		public abstract string EvalRight(Context ctx, string rdest);
+		public abstract void EvalBool(Context ctx, string lbl_true, string lbl_false);
+	}
+
+	public class ExprAss : ExprRoot {
+		public ExprAss(ExprRoot a, ExprRoot b) { this.a = a; this.b = b; }
+		protected ExprRoot a;
+		protected ExprRoot b;
+		public override string EvalLeft(Context ctx)
+		{
+			return "";
+		}
+		public override string EvalRight(Context ctx, string rdest){ return ""; }
+		public override void EvalBool(Context ctx, string lbl_true, string lbl_false) {
+			string s = this.EvalRight(ctx, null);
+			//if (lbl_true != null) ctx.bne(s, "#0");
+			//else if (lbl_false != null) ctx.be(s, "#0");
+			//else Debug.Assert(false);
+		}
+
 	}
 
 	public abstract class ExprBin : ExprRoot
@@ -40,47 +70,95 @@ namespace LLParserGenTest
 		protected ExprBin(ExprRoot a, ExprRoot b) { this.a = a; this.b = b; }
 		protected ExprRoot a;
 		protected ExprRoot b;
+		public override void EvalBool(Context ctx, string lbl_true, string lbl_false) {
+			string s = this.EvalRight(ctx, null);
+			//if (lbl_true != null) ctx.bne(s, "#0");
+			//else if (lbl_false != null) ctx.be(s, "#0");
+			//else Debug.Assert(false);
+		}
 	}
 	public abstract class ExprUni : ExprRoot
 	{
 		protected ExprUni(ExprRoot a) { this.a = a; }
 		protected ExprRoot a;
+		public override void EvalBool(Context ctx, string lbl_true, string lbl_false) {
+			string s = this.EvalRight(ctx, null);
+			//if (lbl_true != null) ctx.bne(s, "#0");
+			//else if (lbl_false != null) ctx.be(s, "#0");
+			//else Debug.Assert(false);
+		}
 	}
 	public class ExprAdd : ExprBin
 	{
 		public ExprAdd(ExprRoot a, ExprRoot b) : base(a, b) {}
-		public override int Exec(Context ctx) { return a.Exec(ctx) + b.Exec(ctx); }
+		public override string EvalRight(Context ctx, string rdest) { return a.EvalRight(ctx, null) + b.EvalRight(ctx, null); }
+		public override string EvalLeft(Context ctx)
+		{
+			return "";
+		}
 	}
 	public class ExprSub : ExprBin
 	{
 		public ExprSub(ExprRoot a, ExprRoot b) : base(a, b) {}
-		public override int Exec(Context ctx) { return a.Exec(ctx) - b.Exec(ctx); }
+		public override string EvalRight(Context ctx, string rdest) { 
+			return a.EvalRight(ctx, null) + "-" + b.EvalRight(ctx, null); 
+		}
+		public override string EvalLeft(Context ctx)
+		{
+			return "";
+		}
 	}
 	public class ExprMul : ExprBin
 	{
 		public ExprMul(ExprRoot a, ExprRoot b) : base(a, b) {}
-		public override int Exec(Context ctx) { return a.Exec(ctx) * b.Exec(ctx); }
+		public override string EvalRight(Context ctx, string rdest) { return a.EvalRight(ctx,null) + "*" + b.EvalRight(ctx,null); }
+		public override string EvalLeft(Context ctx)
+		{
+			return "";
+		}
 	}
 	public class ExprDiv : ExprBin
 	{
 		public ExprDiv(ExprRoot a, ExprRoot b) : base(a, b) {}
-		public override int Exec(Context ctx) { return a.Exec(ctx) / b.Exec(ctx); }
+		public override string EvalRight(Context ctx, string rdest) { return a.EvalRight(ctx,null) + "/" + b.EvalRight(ctx,null); }
+		public override string EvalLeft(Context ctx)
+		{
+			return "";
+		}
 	}
 	public class ExprPlus : ExprUni
 	{
 		public ExprPlus(ExprRoot a) : base(a) {}
-		public override int Exec(Context ctx) { return a.Exec(ctx); }
+		public override string EvalRight(Context ctx, string rdest) { return a.EvalRight(ctx,null); }
+		public override string EvalLeft(Context ctx)
+		{
+			return "";
+		}
 	}
 	public class ExprNeg : ExprUni
 	{
 		public ExprNeg(ExprRoot a) : base(a) {}
-		public override int Exec(Context ctx) { return a.Exec(ctx); }
+		public override string EvalRight(Context ctx, string rdest) { return a.EvalRight(ctx,null); }
+		public override string EvalLeft(Context ctx)
+		{
+			return "";
+		}
 	}
 	public class ExprNum : ExprRoot
 	{
 		public ExprNum(LLParserLexerLib.TokenAST a) { this.a = a; }
 		LLParserLexerLib.TokenAST a;
-		public override int Exec(Context ctx) { return int.Parse(a.v); }
+		public override string EvalRight(Context ctx, string rdest) { return a.v; }
+		public override string EvalLeft(Context ctx)
+		{
+			return "";
+		}
+		public override void EvalBool(Context ctx, string lbl_true, string lbl_false) {
+			string s = this.EvalRight(ctx, null);
+			//if (lbl_true != null) ctx.bne(s, "#0");
+			//else if (lbl_false != null) ctx.be(s, "#0");
+			//else Debug.Assert(false);
+		}
 	}
 
 	////////////////////////
@@ -91,7 +169,7 @@ namespace LLParserGenTest
 			: base(0)
 		{
 		}
-		public ExprRoot Start(LexReader rd)
+		public StmtRoot Start(LexReader rd)
 		{
 			this.init(rd);
 			return this.start(null);
